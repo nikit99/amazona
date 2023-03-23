913473ad8de944e5501573771c0e68a4a96b7dd3
nikit99 <nikitojha99@gmail.com>
2023-03-23 17:25:20 +0530

    video-lect-3-create-react-app

---
+++ test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import moduleName from 'module'
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+function app() {
+  return <div classname="test"> hello world</div>;
+}
+export default app;
